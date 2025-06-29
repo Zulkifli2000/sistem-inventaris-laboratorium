55b99c67598e7796d2c463c4ed4038594ee46e95
Zulkifli2000 <105841107121@student.unismuh.ac.id>
2025-06-29 23:13:05 +0800

    Fix: Tambah JavaScript Bootstrap untuk hamburger navbar responsive

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
   <div class="container">
-    <!-- Brand / Logo -->
-    <a class="navbar-brand" href="/pages/index.php">Laboratorium Komputer</a>
+    <a class="navbar-brand" href="/index.php">Laboratorium Komputer</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="navbar-nav">
-      <a class="nav-link" href="/pages/manajemen/inventaris.php">Inventaris</a>
-      <a class="nav-link" href="/pages/riwayat/riwayat.php">Riwayat</a>
-      <a class="nav-link" href="/pages/manajemen/profile.php">Profile</a>
-      <a class="nav-link" href="/pages/logout.php">Logout</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link" href="/pages/manajemen/inventaris.php">Inventaris</a>
+        <a class="nav-link" href="/pages/riwayat/riwayat.php">Riwayat</a>
+        <a class="nav-link" href="/pages/manajemen/profile.php">Profile</a>
+        <a class="nav-link" href="/pages/logout.php">Logout</a>
+      </div>
     </div>
   </div>
 </nav>
+
+<!-- Tambahkan ini untuk hamburger bisa dibuka -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
